2aca1f21ae84f2a2d13e017ac25f72f355b8ed2a
Sameer Rao <sameerrao@El-MacBook-Air.local>
2026-04-28 00:26:48 -0700

    basic frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.db.database import engine, Base
 from app.model.models import Workout, Exercise
 from app.api.routes import router
@@ -7,8 +9,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(router)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.get("/")
+def index():
+    return FileResponse("app/static/index.html")
